1eba357c23d844d5ff0628ac290f576d59e72673
Jeff Sims <jeffsims@jeffs-mbp.lan>
2023-12-04 08:59:48 -0500

    day 3 part 1

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -17,12 +17,15 @@ struct Symbol {
 }
 
 fn main() {
-    let file = fs::read_to_string(Path::new("./day_03/src/parts_test.txt")).expect("file exists");
-    let result = part_one(&file);
+    let file = fs::read_to_string(Path::new("./day_03/src/parts.txt")).expect("file exists");
+    let part_one_result = part_one(&file);
+    println!("day 3 part 1 result is {part_one_result}");
 }
 
 fn part_one(file: &String) -> i32 {
     let rows: Vec<&str> = file.split("\n").collect();
+    let row_length = rows[0].len();
+    let row_count = rows.len();
     let symbols: Vec<Symbol> = rows
         .clone()
         .into_iter()
@@ -56,7 +59,7 @@ fn part_one(file: &String) -> i32 {
                     let mut value = "".to_owned();
                     let start = row_position;
                     let mut end = row_position;
-                    while chars[end].is_numeric() {
+                    while end < chars.len() && chars[end].is_numeric() {
                         value.push(chars[end]);
                         end += 1;
                     }
@@ -64,7 +67,7 @@ fn part_one(file: &String) -> i32 {
                     found_numbers.push(Number {
                         row: row_index,
                         start: start,
-                        end: end,
+                        end: end - 1,
                         value: value.parse::<i32>().unwrap(),
                     })
                 } else {
@@ -75,9 +78,56 @@ fn part_one(file: &String) -> i32 {
         })
         .collect();
 
-    println!("symbols {:?}", symbols);
-    println!("numbers {:?}", numbers);
-    1
+    let mut result: Vec<i32> = Vec::new();
+    for number in &numbers {
+        let symbol_search_matrix = get_symbol_search_matrix(number, row_length, row_count);
+        for (row, position) in symbol_search_matrix {
+            for symbol in &symbols {
+                if symbol.row == row && symbol.position == position {
+                    result.push(number.value);
+                    break;
+                }
+            }
+        }
+    }
+
+    result.into_iter().sum()
+}
+
+fn get_symbol_search_matrix(
+    number: &Number,
+    row_length: usize,
+    row_count: usize,
+) -> Vec<(usize, usize)> {
+    let mut search_matrix: Vec<(usize, usize)> = Vec::new();
+    let col_start = if number.start == 0 {
+        0
+    } else {
+        number.start - 1
+    };
+    let col_end: usize = if number.end == row_length - 1 {
+        number.end
+    } else {
+        number.end + 1
+    };
+    let row_start = if number.row == 0 {
+        number.row
+    } else {
+        number.row - 1
+    };
+    let row_end = if number.row == row_count - 1 {
+        number.row
+    } else {
+        number.row + 1
+    };
+
+    for row in row_start..=row_end {
+        for col in col_start..=col_end {
+            search_matrix.push((row, col));
+        }
+    }
+
+    search_matrix
 }
 
 #[cfg(test)]
