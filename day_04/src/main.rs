d49e68085b2e5dc1a8641fcce11cb30dfbcfec28
Jeff Sims <jeffsims@jeffs-mbp.lan>
2023-12-06 08:57:25 -0500

    failing day 4 part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
@@ -44,6 +45,53 @@ fn part_one(file: &String) -> i32 {
         .sum()
 }
 
+fn part_two(file: &String) -> i32 {
+    let cards = file.split("\n");
+    cards
+        .map(|c| {
+            let mut card = c.split_terminator(&[':', '|']).collect::<Vec<&str>>();
+            let card_numbers = card
+                .pop()
+                .unwrap()
+                .split_whitespace()
+                .collect::<Vec<&str>>();
+            let winning_numbers = card
+                .pop()
+                .unwrap()
+                .split_whitespace()
+                .collect::<Vec<&str>>();
+            let mut points = 0;
+            for winning_number in winning_numbers {
+                for card_number in card_numbers.to_owned() {
+                    if card_number == winning_number {
+                        points += 1;
+                        break;
+                    }
+                }
+            }
+            points
+        })
+        .collect::<Vec<i32>>()
+        .into_iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut acc, (index, points)| {
+            if points > 0 {
+                *acc.entry((index + 1) as i32).or_insert(0) += 1;
+                let start = index as i32 + 1;
+                let end = start + points;
+                println!("card {} start {} end {}", index + 1, start, end);
+                for card in start..end {
+                    *acc.entry(card + 1).or_insert(0) += 1;
+                }
+            }
+            println!("HashMap {:?}", acc);
+            acc
+        })
+        .into_iter()
+        .map(|(_, cards)| cards)
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,5 +100,12 @@ mod tests {
         let file =
             fs::read_to_string(Path::new("./../day_04/src/scratch_test.txt")).expect("file exists");
         assert_eq!(part_one(&file), 13)
+    }
+
+    #[test]
+    fn day_4_part_two_gets_the_right_value() {
+        let file =
+            fs::read_to_string(Path::new("./../day_04/src/scratch_test.txt")).expect("file exists");
+        assert_eq!(part_two(&file), 30)
     }
 }
